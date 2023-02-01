9f28c48a7a4576e6212fc4f2d8ccd6f4e5b8de4e
wild-valentin <marlois.valentin@gmail.com>
2023-02-01 12:07:51 +0100

    table project and commentaire as created

--- backend/database.sql
+++ backend/database.sql
@@ -4,3 +4,24 @@ CREATE TABLE item (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+
+CREATE TABLE project (
+  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  titre varchar(255) NOT NULL,
+  date_ajout date NOT NULL,
+  image varchar(255) ,
+  description text ,
+  lien varchar(255) NOT NULL,
+  list_techno varchar(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO project (titre , date_ajout , image , description , lien , list_techno ) VALUES 
+("Morpion socket io" ,NOW() ,"frontend/src/assets/morpion.png","Creation d'un morpion multi joueur avec socket io ","https://github.com/M-Valentin-51/socket.io-morpion","html css react express socket-io");
+
+CREATE TABLE commentaire (
+  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nom varchar(255) NOT NULL,
+  date_ajout date NOT NULL,
+  message text
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
